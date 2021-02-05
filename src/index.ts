8ebbf11297d0a8f9a2ff09f3e30e4cfb75bcac76
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-02-04 21:13:08 -0300

    single test using mealBox

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('hello');
+import { MealBox } from './creational/builder/classes/meal-box';
+import { Beans, Meat, Rice } from './creational/builder/classes/meals';
+
+const rice = new Rice('Arroz', 10);
+const beans = new Beans('Feijão', 10);
+const meat = new Meat('Carne', 20);
+const mealBox = new MealBox();
+
+mealBox.addMeal(rice, beans, meat);
+
+console.log(mealBox.getPrice());
