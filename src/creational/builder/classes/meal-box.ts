2358b5ae9901f9b96c4573ee2f3e599c4ef69825
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-02-04 21:12:50 -0300

    create mealBox class with getPrice and addMeal methods

---
+++ src/creational/builder/classes/meal-box.ts
@@ -0,0 +1,12 @@
+import { MealCompositeContract } from '../interfaces/meal-composite-contract';
+
+export class MealBox implements MealCompositeContract {
+  private readonly _children: MealCompositeContract[] = [];
+  getPrice(): number {
+    return this._children.reduce((sum, meal) => sum + meal.getPrice(), 0);
+  }
+
+  addMeal(...newMeal: MealCompositeContract[]): void {
+    newMeal.forEach((item) => this._children.push(item));
+  }
+}
