cdadf28fcfd220e9cc1059fa5fede8e019099eeb
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-02-04 21:11:30 -0300

    create abstractMeal with composite contract implementation

---
+++ src/creational/builder/classes/abstract-meal.ts
@@ -0,0 +1,8 @@
+import { MealCompositeContract } from '../interfaces/meal-composite-contract';
+export abstract class AbstractMeal implements MealCompositeContract {
+  constructor(private name: string, private price: number) {}
+
+  getPrice(): number {
+    return this.price;
+  }
+}
