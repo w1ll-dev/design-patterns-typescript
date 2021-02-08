ce7cac56bb6e278d4c3337e21bd286edd363fd90
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-02-08 20:47:45 -0300

    VeganDishBuilder use

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { MainDishBuilder } from './creational/builder/classes/main-dish-builder';
+import { VeganDishBuilder } from './creational/builder/classes/vegan-dish-builder';
 
 const mainDishBuilder = new MainDishBuilder();
 mainDishBuilder.makeDish().makeDessert();
@@ -7,3 +8,8 @@ mainDishBuilder.reset();
 
 const meal2Price = mainDishBuilder.makeBeverage().getPrice();
 console.log(meal2Price);
+
+const veganDishBuilder = new VeganDishBuilder();
+veganDishBuilder.makeDish();
+console.log(veganDishBuilder.getMeal());
+console.log(veganDishBuilder.getPrice());
