6100fd508883abe763678fed1d82e698d7f20329
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-04-05 09:33:46 -0300

    create devices with device implementation

---
+++ src/structural/bridge/device/radio.ts
@@ -0,0 +1,28 @@
+import { DeviceImplementation } from './device-implementation';
+
+export class Tv implements DeviceImplementation {
+  private volum = 10;
+  private power = false;
+  private name = 'Tv';
+
+  getName(): string {
+    return this.name;
+  }
+
+  setPower(newPowerStatus: boolean): void {
+    this.power = newPowerStatus;
+  }
+
+  getPower(): boolean {
+    return this.power;
+  }
+
+  setVolum(volum: number): void {
+    if (volum < 0 || volum > 100) return;
+    this.volum = volum;
+  }
+
+  getVolum(): number {
+    return this.volum;
+  }
+}
